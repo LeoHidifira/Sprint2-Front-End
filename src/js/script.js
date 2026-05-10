71064de2d53dde71e39c4ca158ecdd74f128571e
Leonardo Hidifira <yuitileonardo@gmail.com>
2026-05-09 21:30:33 -0300

    feat: Adicionando a funcionalidade do menu hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
+const menuBtn = document.getElementById("menu-btn");
+const nav = document.querySelector("nav");
+
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
+
 const swiper = new Swiper(".swiper", {
     loop: true,
 
